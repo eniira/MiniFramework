d6c8ca7709a72f4198ebc7dd7427606ad79785db
eniira <catarinecruz@alunos.utfpr.edu.br>
2023-04-29 19:21:49 -0300

    implementando models

--- Models.cs
+++ Models.cs
@@ -1,7 +1,53 @@
-
-namespace Models{
+namespace Models
+{
     class CharFild
     {
-        
+        public int tamanho {get; set}
+        public string verboso {get; set}
+        public bool nulo {get;set}
+
+        public CharFild(int tamanho = 255, string verboso = '', bool nulo = false)
+        {
+            this.tamanho = tamanho;
+            this.verboso = verboso;
+            this.nulo = nulo;
+        }
+    }
+
+    class TextFild
+    {
+        public string verboso {get; set}
+        public bool nulo {get;set}
+
+        public TextFild(string verboso = '', bool nulo = false)
+        {
+
+            this.verboso = verboso;
+            this.nulo = nulo;
+        }
+    }
+
+    class DataFild
+    {
+        public string verboso {get; set}
+        public bool nulo {get;set}
+
+        public DataFild(string verboso = '', bool nulo = false)
+        {
+            this.verboso = verboso;
+            this.nulo = nulo;
+        }
+    }
+
+    class IntegerFild
+    {
+        public string verboso {get; set}
+        public bool nulo {get;set}
+
+        public integerFild(string verboso = '', bool nulo = false)
+        {
+            this.verboso = verboso;
+            this.nulo = nulo;
+        } 
     }
 }
