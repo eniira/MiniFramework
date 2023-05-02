4d9fbc0c94f6425570e7541152f5fcb5310538cc
Thaynara Santos <thaynara.santos@mznsolucoes.com.br>
2023-05-02 00:54:32 -0300

    fazendo formulario

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,63 +7,73 @@ using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Text;
 using System.Threading.Tasks;
+using MineFramework.Models; // adiciona o namespace que contém a classe Form
+
 
 namespace MiniFramework.Controllers;
 
+public class Pessoa {
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+}
+
 public class HomeController : Controller
 {
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        [HttpGet]
-        public IActionResult Cadastrar(int? id)
-        {                                  
-            if (id.HasValue && Usuario.Listagem.Any(u => u.IdUsuario == id))
+    [HttpGet]
+    public IActionResult Cadastrar(int? id)
+    {
+        if (id.HasValue && Usuario.Listagem.Any(u => u.IdUsuario == id))
+        {
+            var usuario = Usuario.Listagem.Single(u => u.IdUsuario == id);
+            return View(usuario);
+        }
+        return View();
+    }
+
+ 
+    public IActionResult Form() {
+        List<Type> types = new List<Type>();
+        types.Add(typeof(Pessoa));
+        MineFramework.Models.Form.GenerateForms(types);
+        return View("Cadastrar");
+    }
+    
+    
+
+    [HttpPost]  
+    public IActionResult Cadastrar(Usuario usuario)
+    {
+       
+        Usuario.Salvar(usuario);
+        using (var conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;"))
+        {
+            conn.Open();
+            using (var command = conn.CreateCommand())
             {
-                var usuario = Usuario.Listagem.Single(u => u.IdUsuario == id); 
-                return View(usuario);
+                string tabela = "CREATE TABLE IF NOT EXISTS Test (id INTEGER PRIMARY KEY AUTOINCREMENT,nome VARCHAR(30),email VARCHAR(100))";
+                command.CommandText = tabela;
+                command.ExecuteNonQuery();
+
+                string sql = "INSERT INTO Test (nome, email) VALUES (@nome, @email)";
+                command.CommandText = sql;
+                // command.Parameters.AddWithValue("id", usuario.IdUsuario);
+                 command.Parameters.AddWithValue("@nome", usuario.Nome);
+                 command.Parameters.AddWithValue("@email", usuario.Email); 
+                command.ExecuteNonQuery();
+                Console.WriteLine(usuario.Nome);
             }
-            return View();
         }
 
-        /*[HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
-        {
-            Usuario.Salvar(usuario);
-            return RedirectToAction("Usuarios");
-        }*/
-
-        [HttpPost]
-        public IActionResult Cadastrar(Usuario usuario)
-        {   
-            using (var conn = new SQLiteConnection("Data Source=database.db;Version=3;New=True;Compress=True;"))
-            {
-                conn.Open();
-                using(var command = conn.CreateCommand())
-                {   
-                    //string criar = "CREATE DATABASE IF NOT EXISTS db";
-                    //command.CommandText = criar;
-                    //command.ExecuteNonQuery();
-                    string tabela = "CREATE TABLE IF NOT EXISTS Test (id INTEGER PRIMARY KEY AUTOINCREMENT,nome VARCHAR(30),email VARCHAR(100))";
-                    //command.CommandText = ""CREATE TABLE IF NOT EXISTS VAR_NAME (id INTEGER PRIMARY KEY AUTOINCREMENT, VAR_FIELDS)"";
-                    command.CommandText = tabela;
-                    command.ExecuteNonQuery();
-                    //command.CommandText = ""INSERT INTO VAR_NAME (VAR_NAMES) VALUES (VAR_VALUES)"";
-                    //VAR_COMMAND_PARAMS
-                    command.CommandText = @"INSERT INTO Test (nome,email) VALUES (@getNome, @getEmail)";
-                    command.Parameters.AddWithValue("getNome", usuario.Nome);
-                    command.Parameters.AddWithValue("getEmail", usuario.Email);
-                    command.ExecuteNonQuery();
-                }
-            }
-
-            return Ok("Dados salvos com sucesso!");
-        }
-
-        public IActionResult Usuarios()
-        {
-            return View(Usuario.Listagem);
-        }
+        return Ok(usuario.Nome);
+    }
+    public IActionResult Usuarios()
+    {
+        return View(Usuario.Listagem);
+    }
 }
